c597a9198a9f3c5b15c331a3a4ca342a5f3023ed
sasi12k <sasi12k@email.com>
2025-02-21 21:40:41 +0700

    done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    double sum = 0, result , count ;
+    if(argc <= 1){
+        cout << "Please input numbers to find average.";
+    }else{
+        for(int i = 0;i < argc;i++){
+            sum += atoi(argv[i]);
+            count = i;
+        }result = sum/count;
+        cout << "---------------------------------\n";
+        cout << "Average of " << count << " numbers = " << result << "\n";
+        cout << "---------------------------------";
+    }
+    return 0;
+}
